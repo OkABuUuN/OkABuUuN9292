6dc78d947b4ed426d5a74f2e608f3ba891a6ad3c
OkABuUuN <168494761+OkABuUuN@users.noreply.github.com>
2024-06-25 10:23:37 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -6,16 +6,16 @@ alert(Math.sqrt(1+1));
 
 alert(Math.sqrt(2+3));
 
-alert(1/0)
+alert(1/0);
 
-alert(0/0)
+alert(0/0);
 
-alert(2+3)
+alert(2+3);
 
-alert('2'+'3')
+alert('2'+'3');
 
-alert(Number('2')+Number('3')
+alert(Number('2')+Number('3');
 
-alert('2'+3)
+alert('2'+3);
 
-alert(Number('2')+3)
+alert(Number('2')+3);
